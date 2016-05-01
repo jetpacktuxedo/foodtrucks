a978212b4ebdb06e081bfba085bcb5b6a5b444b2
jetpacktuxedo <crazeh.monkeh@gmail.com>
2016-05-01 08:12:11 -0700

    Fixed today/tomorrow so they work

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ def full_schedule():
 def today():
     schedule = fetch.schedule()
     today = datetime.date.today().isoformat()
-    my_item = [item.get('date') for item in schedule if item.get('date') == today]
+    my_item = [item for item in schedule if item.get('date') == today]
     resp = flask.make_response(json.dumps(my_item))
     resp.status='200'
     resp.mimetype="application/json"
@@ -26,7 +26,7 @@ def today():
 def tomorrow():
     schedule = fetch.schedule()
     tomorrow = (datetime.date.today() + datetime.timedelta(days=1)).isoformat()
-    my_item = [item.get('date') for item in schedule if item.get('date') == tomorrow]
+    my_item = [item for item in schedule if item.get('date') == tomorrow]
     resp = flask.make_response(json.dumps(my_item))
     resp.status='200'
     resp.mimetype="application/json"
